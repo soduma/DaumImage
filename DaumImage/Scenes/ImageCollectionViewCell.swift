259b46ac3386e8d6c1334f52eda8c6a1d5c540b3
soduma <soduma@naver.com>
2022-04-05 15:05:04 +0900

    data fetch

--- DaumImage/Scenes/ImageCollectionViewCell.swift
+++ DaumImage/Scenes/ImageCollectionViewCell.swift
@@ -10,9 +10,23 @@ import Kingfisher
 import SnapKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
+    private lazy var photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        
+        contentView.addSubview(photoImageView)
+        photoImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func fetch(imageURL: String, thumbnailURL: String) {
+        guard let url = URL(string: imageURL) else { return }
+        photoImageView.kf.setImage(with: url)
     }
 }
--- DaumImage/Scenes/ImageViewController.swift
+++ DaumImage/Scenes/ImageViewController.swift
@@ -10,6 +10,9 @@ import SnapKit
 
 class ImageViewController: UIViewController {
     let networkManager = NetworkManager()
+    var imageList: [Documents]?
+    let keyword: String
+    var currentPage = 1
     
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -25,20 +28,38 @@ class ImageViewController: UIViewController {
         collectionView.delegate = self
         return collectionView
     }()
-
+    
+    init(keyword: String) {
+        self.keyword = keyword
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .brown
-//        networkManager.getImage(keyword: "아이유", page: 50)
-        
         layout()
+        
+        Task {
+            await fetch()
+        }
     }
     
     func layout() {
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+    }
+    
+    func fetch() async {
+        let result = await networkManager.getImage(keyword: keyword, page: currentPage)
+        imageList = result
+        if let imageList = imageList {
+            print(imageList)
         }
     }
 }
